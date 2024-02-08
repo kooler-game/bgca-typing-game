63f5d1b03999562f5daadb621fed632c8cc73878
Kooler FAN <koolerkx@gmail.com>
2024-02-08 20:28:15 +0800

    Implment accept user input

--- Assets/Scripts/TypingManager.cs
+++ Assets/Scripts/TypingManager.cs
@@ -3,18 +3,44 @@ using TMPro;
 
 public class TypingManager : MonoBehaviour
 {
-    string[] word = { "hello", "bye", "happy new year" };
+    string[] word = { "hello", "bye", "happy" };
 
     [SerializeField]
     TextMeshProUGUI text;
+
+    private int _answerPtr = 0;
+    private int randomIndex;
 
     void Start()
     {
         ShowNewText();
     }
 
-    void ShowNewText() {
-        int randomIndex = Random.Range(0, word.Length);
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (Input.GetKeyDown(word[randomIndex][_answerPtr].ToString()))
+            {
+                // Correct
+                _answerPtr++;
+
+                if (_answerPtr >= word[randomIndex].Length)
+                {
+                    ShowNewText();
+                }
+            }
+            else
+            {
+                // Incorrect
+            }
+        }
+    }
+
+    void ShowNewText()
+    {
+        _answerPtr = 0;
+        randomIndex = Random.Range(0, word.Length);
         text.text = word[randomIndex];
     }
 }
