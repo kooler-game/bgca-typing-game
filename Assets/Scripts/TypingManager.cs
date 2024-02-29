ea94db84e1ecd6307954c090133d31909afe58cd
Kooler FAN <koolerkx@gmail.com>
2024-02-29 21:07:30 +0800

    Implement current typed word count

--- Assets/Scripts/TypingManager.cs
+++ Assets/Scripts/TypingManager.cs
@@ -11,6 +11,13 @@ public class TypingManager : MonoBehaviour
     [SerializeField]
     TextAsset textFileSource;
 
+    public int targetWord = 10;
+    private int wordTyped = 0;
+
+    private bool inGame = true;
+    public GameObject gameoverPanel;
+    public TextMeshProUGUI typedText;
+
     private int _answerPtr = 0;
     private int randomIndex;
 
@@ -18,11 +25,12 @@ public class TypingManager : MonoBehaviour
     {
         ReadTextFile(textFileSource);
         ShowNewText();
+        SetScoreText(0, targetWord);
     }
 
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (inGame && Input.anyKeyDown)
         {
             if (Input.GetKeyDown(word[randomIndex][_answerPtr].ToString()))
             {
@@ -30,21 +38,40 @@ public class TypingManager : MonoBehaviour
                 _answerPtr++;
 
                 // Show correct color response
-                text.text =  "<color=#C5E90B>" + word[randomIndex].Substring(0, _answerPtr) + "</color>" + word[randomIndex].Substring(_answerPtr);
+                text.text = "<color=#C5E90B>" + word[randomIndex].Substring(0, _answerPtr) + "</color>" + word[randomIndex].Substring(_answerPtr);
 
                 if (_answerPtr >= word[randomIndex].Length)
                 {
-                    ShowNewText();
+                    Correct();
                 }
             }
             else
             {
                 // Incorrect
-                text.text =  "<color=#C5E90B>" + word[randomIndex].Substring(0, _answerPtr) + "</color>" 
-                            + "<color=#FF0000>" + word[randomIndex].Substring(_answerPtr, 1) + "</color>"  
+                text.text = "<color=#C5E90B>" + word[randomIndex].Substring(0, _answerPtr) + "</color>"
+                            + "<color=#FF0000>" + word[randomIndex].Substring(_answerPtr, 1) + "</color>"
                             + word[randomIndex].Substring(_answerPtr + 1);
             }
         }
+    }
+
+    void Correct()
+    {
+        ShowNewText();
+        wordTyped++;
+        SetScoreText(wordTyped, targetWord);
+
+        if (wordTyped >= targetWord)
+        {
+            // Game End
+            inGame = false;
+            gameoverPanel.SetActive(true);
+        }
+    }
+
+    void SetScoreText(int current, int target)
+    {
+        typedText.text = "Typed: " + current.ToString() + "/" + target.ToString();
     }
 
     void ShowNewText()
