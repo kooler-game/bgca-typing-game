9b30ad9b56bf4df9cda22a2d5f6fa4fe7e65141d
Kooler FAN <koolerkx@gmail.com>
2024-04-15 20:37:37 +0800

    Implement ranking save

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,7 +2,15 @@ using UnityEngine;
 
 public static class ScoreManager
 {
+    public readonly static string wpmSaveDataKey = "wpm";
     public static float[] rankingWPM = { 0, 0, 0, 0, 0 };
+
+    static ScoreManager()
+    {
+        rankingWPM = LoadScore();
+        Debug.Log("Constructor");
+        PrintScore(rankingWPM);
+    }
 
     public static void EndGame(float wpm)
     {
@@ -23,9 +31,54 @@ public static class ScoreManager
             }
         }
 
-        for (int i = 0; i < rankingWPM.Length; i++)
+        PrintScore(rankingWPM);
+        SaveScore(rankingWPM);
+    }
+
+    private static void SaveScore(float[] wpms)
+    {
+        SaveData saveData = new SaveData
         {
-            Debug.Log(i + ": " + rankingWPM[i]);
+            wpms = wpms
+        };
+
+        PlayerPrefs.SetString(wpmSaveDataKey, saveData.ToJson());
+    }
+
+    private static float[] LoadScore()
+    {
+        string saveDataJsonString = PlayerPrefs.GetString(wpmSaveDataKey);
+        SaveData saveData = JsonUtility.FromJson<SaveData>(saveDataJsonString);
+        Debug.Log("Load Score");
+        PrintScore(saveData.wpms);
+        return saveData.wpms;
+    }
+
+
+    // For Debug
+    private static void PrintScore(float[] wpms)
+    {
+        string message = "";
+        message += "WPM\n";
+        for (int i = 0; i < wpms.Length; i++)
+        {
+            message += i + ": " + wpms[i] + "\n";
         }
+        Debug.Log(message);
+    }
+}
+
+class SaveData
+{
+    public float[] wpms = { 0, 0, 0, 0, 0 };
+
+    public string ToJson()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public static SaveData FromJson(string json)
+    {
+        return JsonUtility.FromJson<SaveData>(json);
     }
 }
