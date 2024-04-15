d7a5f7d73b5e3bcf133f1889cbd8a018dc6500d8
Kooler FAN <koolerkx@gmail.com>
2024-04-15 21:15:08 +0800

    Implement score ranking reflect to UI

--- Assets/Scripts/MainScreenManager.cs
+++ Assets/Scripts/MainScreenManager.cs
@@ -1,10 +1,35 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainScreenManager : MonoBehaviour
 {
+    public GameObject mainMenuPanel;
+    public GameObject rankingMenuPanel;
+    public TextMeshProUGUI[] rankingItemWpms;
+
     public static void StartGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    public void OnClickRankingMenu()
+    {
+        // Set ranking
+        float[] wpms = ScoreManager.rankingWPM;
+
+        for (int i = 0; i < wpms.Length; i++)
+        {
+            rankingItemWpms[i].text = (i + 1).ToString() + ". " + wpms[i].ToString("0.00");
+        }
+
+        mainMenuPanel.SetActive(false);
+        rankingMenuPanel.SetActive(true);
+    }
+
+    public void OnClickBackToMainMenu()
+    {
+        rankingMenuPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
     }
 }
